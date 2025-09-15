3d25b41b6f9acf4ccd5c3d96b9f1392277ca1c7a
Philippe MICHEL <philippe@docteur-michel.fr>
2025-09-15 13:27:27 +0200

    Patients en cours

--- import.R
+++ import.R
@@ -13,26 +13,28 @@ library("janitor")
 library("labelled")
 library("baseph")
 #
-patients <- read_ods("./datas/patients.ods", na = c("", " ", "NA")) |>
+nax <- c("", " ", "NA","D")
+
+patients <- read_ods("./datas/patients.ods", na = nax) |>
   clean_names() |>
   mutate(across(where(is.character), as.factor))
-bn <- read_ods("./datas/patients.ods", sheet = "bnom", na = c("", " ", "NA"))
+bn <- read_ods("./datas/patients.ods", sheet = "bnom", na = nax)
 var_label(patients) <- bn$titres
-patients <- remove_constant(patients)
+patients <- remove_constant(patients, na.rm = TRUE)
 #
-bloc <- read_ods("./datas/bloc.ods", na = c("", " ", "NA")) |>
+bloc <- read_ods("./datas/bloc.ods", na = nax) |>
   clean_names() |>
   mutate(across(where(is.character), as.factor))
-bn <- read_ods("./datas/bloc.ods", sheet = "bnom", na = c("", " ", "NA"))
+bn <- read_ods("./datas/bloc.ods", sheet = "bnom", na =  nax)
 var_label(bloc) <- bn$titres
-bloc <- remove_constant(bloc)
+bloc <- remove_constant(bloc, na.rm = TRUE)
 #
-tt <- read_ods("./datas/traitement.ods", na = c("", " ", "NA")) |>
+tt <- read_ods("./datas/traitement.ods", na = nax) |>
   clean_names() |>
   mutate(across(where(is.character), as.factor))
-bn <- read_ods("./datas/traitement.ods", sheet = "bnom", na = c("", " ", "NA"))
+bn <- read_ods("./datas/traitement.ods", sheet = "bnom", na = nax)
 var_label(tt) <- bn$titres
-tt <- remove_constant(tt)
+tt <- remove_constant(tt, na.rm = TRUE)
 
 
 rm(bn)
